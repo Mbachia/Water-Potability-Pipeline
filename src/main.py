aadc0a4860e26aadc688c1a80450b7ef7a6336b3
Mbachia Ngethe <espymbachia13@gmail.com>
2024-08-20 19:21:15 +0300

    Updates on API

--- src/main.py
+++ src/main.py
@@ -1,6 +1,8 @@
 from fastapi import FastAPI
 import pickle
 import pandas as pd
+from data_model import BaseModel
+from data_model import Water
 
 app = FastAPI(
     title="Water Potability Prediction",
@@ -14,3 +16,23 @@ with open(r"C:\Users\espym.LAPTOP-41F90NSA\projects\ml_pipeline\model.pkl", "rb"
 def index():
     return "Welcome to water Potability Prediction FastAPI"
 
+app.post("/predict")
+def model_preedict(water: Water):
+    sample = pd.DataFrame({
+        'ph' : [water.ph],
+        'Hardness' : [water.Hardness],
+        'Solids' :[water.Hardness],
+        'Chloramines' : [water.Chloramines],
+        'Sulfate' : [water.Sulfate],
+        'Conductivity' : [water.Conductivity],
+        'Organic_carbon': [water.Organic_carbon],
+        'Trihalonethanses': [water.Trihalonethanses],
+        'Turbidity' : [water.Turbidity]
+    })
+
+    predictions = model.load(sample)
+
+    if predictions == 1:
+        return "water is consumable"
+    else:
+        return "water is not consumable"
