de4d457f47a66231e434af85164fb2e645f69df1
Mbachia <espymbachia13@gmail.com>
2024-09-04 08:43:20 +0300

    Conversion of code into modular code. Adding Error and exception handling and type hinting.

--- src/model_eval.py
+++ src/model_eval.py
@@ -3,29 +3,79 @@ import pandas as pd
 
 import pickle
 import json
+from typing import Tuple
 
-from sklearn.metrics import accuracy_score,f1_score,precision_score,recall_score
+from sklearn.metrics import accuracy_score,precision_score,recall_score,f1_score
 
-test_data = pd.read_csv(r'data\processed\test_processed.csv')
+def load_data(filepath : str) -> pd.DataFrame:
+    try:
+         return pd.read_csv(filepath)
+    except Exception as e:
+        raise Exception(f"Error loading data from {filepath}:{e}")
+#test_data = pd.read_csv("./data/processed/test_processed.csv")
 
-X_test = test_data.iloc[:,:-1].values
-y_test = test_data.iloc[:,-1].values
+# X_test = test_data.iloc[:,0:-1].values
+# y_test= test_data.iloc[:,-1].values
 
-model = pickle.load(open("model.pkl", 'rb'))
+def prepare_data(data: pd.DataFrame) -> Tuple[pd.DataFrame, pd.Series]:
+    try:
+        X = data.drop(columns=['Potability'],axis=1)
+        y = data['Potability']
+        return X,y
+    except Exception as e:
+        raise Exception(f"Error Preparing data:{e}")
 
-y_pred = model.predict(X_test)
 
-acc = accuracy_score(y_test,y_pred)
-f1 = f1_score(y_test,y_pred)
-precision = precision_score(y_test,y_pred)
-recall = recall_score(y_test,y_pred)
+def load_model(filepath:str):
+    try:
+        with open(filepath,"rb") as file:
+            model= pickle.load(file)
+        return model
+    except Exception as e:
+        raise Exception(f"Error loading model from {filepath}:{e}")
+    
+#model = pickle.load(open("model.pkl","rb"))
+def evaluation_model(model, X_test:pd.DataFrame, y_test:pd.Series) -> dict:
+    try:
+        y_pred = model.predict(X_test)
 
-metrics_dict = {
-    'acc': acc,
-    'precision' : precision,
-    'f1_score': f1,
-    'recall_score': recall
-}
+        acc = accuracy_score(y_test,y_pred)
+        pre = precision_score(y_test,y_pred)
+        recall = recall_score(y_test,y_pred)
+        f1score = f1_score(y_test,y_pred)
 
-with open('metrics.json','w') as file:
-    json.dump(metrics_dict,file,indent=4)
+        metrics_dict = {
+
+            'acc':acc,
+            'precision':pre,
+            'recall' : recall,
+            'f1_score': f1score
+        }
+        return metrics_dict
+    except Exception as e:
+        raise Exception(f"Error evaluating model : {e}")
+
+
+def save_metrics(metrics_dict:dict,filepath:str) -> None:
+    try:
+        with open('metrics.json','w') as file:
+            json.dump(metrics_dict,file,indent=4)
+    except Exception as e:
+        raise Exception(f"Error saving metrics to {filepath}:{e}")
+    
+def main():
+    try:
+        test_data_path = "./data/processed/test_processed.csv"
+        model_path = "model.pkl"
+        metrics_path = "metrics.json"
+
+        test_data = load_data(test_data_path)
+        X_test,y_test = prepare_data(test_data)
+        model = load_model(model_path)
+        metrics = evaluation_model(model,X_test,y_test)
+        save_metrics(metrics,metrics_path)
+    except Exception as e:
+        raise Exception(f"An Error occurred:{e}")
+
+if __name__ == "__main__":
+    main()
