9cd0da0ad049ec90a2633de989582d1c84902314
Mbachia <espymbachia13@gmail.com>
2024-09-04 08:24:42 +0300

    Error and exception handling

--- src/data_collection.py
+++ src/data_collection.py
@@ -1,19 +1,63 @@
 import pandas as pd
 import numpy as np
-from sklearn.model_selection import train_test_split
 import os
+from sklearn.model_selection import train_test_split
 import yaml
+from typing import Tuple
 
 
-data = pd.read_csv(r"archive\water_potability.csv")
+def load_params(filepath : str) -> float:
+    try:
+        with open(filepath,"r") as file:
+            params = yaml.safe_load(file)
+        return params["data_collection"]["test_size"]
+    except Exception as e:
+        raise Exception(f"Error loading parameters from {filepath}:{e}")
 
-test_size = yaml.safe_load(open("params.yaml"))["data_collection"]["test_size"]
 
-train_data,test_data = train_test_split(data, test_size=test_size, random_state=42)
+#test_size = yaml.safe_load(open("params.yaml"))["data_collection"]["test_size"]
 
-data_path = os.path.join("data", "raw")
+def load_data(filepath : str) -> pd.DataFrame :
+    try:
+        return pd.read_csv(filepath)
+    except Exception as e:
+        raise Exception(f"Error loading data from {filepath} :{e}")
 
-os.makedirs(data_path)
+# data = pd.read_csv(r"C:\Users\SFL-3\water_potability.csv")
 
-train_data.to_csv(os.path.join(data_path, "train.csv"),index=False)
-test_data.to_csv(os.path.join(data_path, "test.csv"),index=False)
+def split_data(data: pd.DataFrame, test_size: float) -> Tuple[pd.DataFrame, pd.DataFrame]:
+    try:
+        return train_test_split(data, test_size= test_size, random_state=42)
+    except Exception as e:
+        raise Exception(f"Error splittin data : {e}")
+
+#train_data, test_data = train_test_split(data, test_size= test_size, random_state=42)
+
+
+
+def save_data(df : pd.DataFrame, filepath: str) -> None:
+    try:
+        df.to_csv(filepath,index=False)
+    except Exception as e:
+        raise Exception(f"Error saving data to {filepath} :{e}")
+    
+
+def main():
+    data_filepath = r"C:\Users\User\Desktop\projects\Water-Potability-Pipeline\archive\water_potability.csv"
+    params_filepath = "params.yaml"
+    raw_data_path = os.path.join("data","raw")
+# data_path = os.path.join("data","raw")
+    try:
+        data = load_data(data_filepath)
+        test_size = load_params(params_filepath)
+        train_data,test_data = split_data(data, test_size)
+
+        os.makedirs(raw_data_path)
+
+        save_data(train_data,os.path.join(raw_data_path,"train.csv"))
+        save_data(test_data, os.path.join(raw_data_path,"test.csv"))
+    except Exception as e:
+        raise Exception(f"An error occurred :{e}")
+    
+if __name__ == "__main__":
+    main()
